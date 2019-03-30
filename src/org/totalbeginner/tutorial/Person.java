ef7f1077fea7755becf6e5dbb9038ef0d47939f2
Java Roadmap <javaroadmap@gmail.com>
2019-03-30 14:20:22 +0900

    1st commit

---
+++ src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,12 @@
+package org.totalbeginner.tutorial;
+
+public  class  Person {
+    //fields
+    private  String  name;  //name of person
+    private  int  maxmimumBooks;   //most books the person can check out
+    //constructors
+    public  Person() {
+        name = "unknown name";
+        maxmimumBooks = 3;
+    }
+}
