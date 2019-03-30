a842fb992e8c7d8bdd74ee737eb65f02bf5e451b
Java Roadmap <javaroadmap@gmail.com>
2019-03-30 15:26:23 +0900

    scrapbook added

--- src/org/totalbeginner/tutorial/Person.java
+++ src/org/totalbeginner/tutorial/Person.java
@@ -1,5 +1,4 @@
 package org.totalbeginner.tutorial;
-
 public  class  Person {
     //fields
     private  String  name;  //name of person
@@ -7,6 +6,18 @@ public  class  Person {
     //constructors
     public  Person() {
         name = "unknown name";
-        maxmimumBooks = 3;
-    }
+        maxmimumBooks = 3; }
+    //methods
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMaxmimumBooks() {
+		return maxmimumBooks;
+	}
+	public void setMaxmimumBooks(int maxmimumBooks) {
+		this.maxmimumBooks = maxmimumBooks;
+	}   
 }
